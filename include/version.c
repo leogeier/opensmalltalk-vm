8ca51ec7fcbb46faf256eb474af72573d9324374
leogeier <contact@leonardgeier.com>
2025-08-12 18:11:25 +0200

    move external build files into repo

---
+++ include/version.c
@@ -0,0 +1,4 @@
+int vm_serial= 1;
+char *vm_date= "Fri Feb 14 14:10:26 UTC 2025";
+char *cc_version= "probably the android ndk aarch64 compiler";
+char *ux_version= "Linux swacopilot 5.4.0-200-generic #220-Ubuntu SMP Fri Sep 27 13:19:16 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux";
